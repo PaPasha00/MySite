b942a5d48e0e86067fffb199027cf383921eeb55
Pavel Vasev <94459147+PaPasha00@users.noreply.github.com>
2022-10-30 23:58:49 +0300

    Add files via upload

---
+++ src/js/myScript.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+  
+  $('.preloader').addClass('stop');
+  
+  let options = {threshold: [0.5]};
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = $('.element-animation');
+    elements.each((i, el) => {
+      observer.observe(el);
+    });
+  
+  
+  
+$(window).scroll(() => {
+  let scrollDistance = $(window).scrollTop();
+  
+    $(".section").each((i, el) => {
+
+      if($(el).offset().top - $("nav").outerHeight() <= scrollDistance){
+        $("nav a").each((i, el) => {
+            if ($(el).hasClass("active")){
+                $(el).removeClass("active");
+            }            
+        });
+
+        $('nav li:eq('+ i +')').find('a').addClass('active');
+      }
+    });
+  });
+ 
+  function calc(){
+    let sums = parseInt($("#typeSite option:selected").val()) + parseInt($("#adapt option:selected").val()) + parseInt($("#design option:selected").val());
+    let dayss = parseInt($("#typeSite option:selected").attr("days")) + parseInt($("#adapt option:selected").attr("days")) + parseInt($("#design option:selected").attr("days"));
+    $(".timers .val").text(dayss);
+    $(".costss .Val").text(sums);
+  }
+  
+  $("select").on("change", function(){
+    calc();
+  });
+  calc();
+  
+  
+  let cli = 0;
+  let hours = 0;
+  let proj = 0;
+  let awar = 0;
+  let fCli = parseInt($("#clients").attr("value"));
+  let fHours = parseInt($("#Hours").attr("value"));
+  let fProj = parseInt($("#Projects").attr("value"));
+  let fAwar = parseInt($("#Awards").attr("value"));
+  
+  function runner(){
+    if (cli < fCli){
+      cli += 1;
+      $(".clients ").text(cli); 
+    }
+    if (hours < fHours){
+      hours += 1;
+      $(".Hours ").text(hours);
+    }
+    if (proj < fProj){
+      proj += 1;
+      $(".Projects ").text(proj);
+    }
+    if (awar < fAwar){
+      awar += 1;
+      $(".Awards ").text(awar);
+    } 
+  };
+  
+  let inter = setInterval(runner, 10);
+  
+  if (hours == fHours){
+    clearInterval("inter");
+  }
+  
+  
+});
+
+
+function onEntry (entry){
+  entry.forEach(change => {
+    if (change.isIntersecting){
+      change.target.classList.add('show-animation');
+    }
+  });
+}  
+
+
+
+
+
